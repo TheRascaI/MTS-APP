67cfdf8d5f58e701cf980664718150ddc29f974f
TheRascaI <70896070+TheRascaI@users.noreply.github.com>
2021-09-17 17:12:46 +0200

    Update UserItem.js

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -3,7 +3,7 @@ import Avatar from "../../shared/components/UIElements/Avatar";
 import './UserItem.css';
 
 const UserItem = props => {
-    console.log(props.image);
+ 
     return(<div id={props.id} className="user-item">
          <div><Avatar image={process.env.REACT_APP_ASSETS_URL+`/${props.image}`} alt="Avatar" width="80px" height="80px" /></div>
         <div><p>id#{props.id}</p><h2>{props.name}</h2><p>Role: {props.role}</p></div>
