b8bf783f2b402c669179296fd1371a0ada214050
TheRascaI <70896070+TheRascaI@users.noreply.github.com>
2021-09-19 18:47:38 +0200

    Update Avatar.js

--- src/shared/components/UIElements/Avatar.js
+++ src/shared/components/UIElements/Avatar.js
@@ -3,10 +3,12 @@ import React from 'react';
 import './Avatar.css';
 
 const Avatar = props => {
+  
+  const imgSrc = `https://avatars.dicebear.com/api/initials/${props.name}.svg`;
   return (
     <div className={`avatar ${props.className}`} style={props.style}>
       <img
-        src={props.image || `https://avatars.dicebear.com/api/initials/${props.name}.svg` }
+        src={imgSrc}
         alt={props.alt}
         style={{ width: props.width, height: props.width }}
       />
