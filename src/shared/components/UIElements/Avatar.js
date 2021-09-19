89f154351d58d1066843aeb7477336796953f434
TheRascaI <70896070+TheRascaI@users.noreply.github.com>
2021-09-19 18:39:33 +0200

    Update Avatar.js

--- src/shared/components/UIElements/Avatar.js
+++ src/shared/components/UIElements/Avatar.js
@@ -6,7 +6,7 @@ const Avatar = props => {
   return (
     <div className={`avatar ${props.className}`} style={props.style}>
       <img
-        src={props.image}
+        src={props.image || `https://avatars.dicebear.com/api/initials/${props.name}.svg` }
         alt={props.alt}
         style={{ width: props.width, height: props.width }}
       />
